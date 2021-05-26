565778f9d887d9ec8288e6bbf548d021eba97235
YGenon <genonster@gmail.com>
2021-05-26 13:38:48 +0300

    Добавьте файлы проекта.

---
+++ WebStore/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+
+            //var greetings = Configuration["Greetings"];
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/greetings", async context =>
+                {
+                    //await context.Response.WriteAsync(greetings);
+                    await context.Response.WriteAsync(Configuration["Greetings"]);
+                });
+
+                //endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
