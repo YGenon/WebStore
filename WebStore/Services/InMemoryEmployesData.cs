97b27d9328b9fc116cb0e3c7e7900208822dca02
YGenon <genonster@gmail.com>
2021-06-11 00:24:04 +0300

    Подключил представления (детали, добавить, редактировать) для работы с Сотрудниками. Не работает представление - удалить и не сохраняются данные в базе данных

--- WebStore/Services/InMemoryEmployesData.cs
+++ WebStore/Services/InMemoryEmployesData.cs
@@ -1,112 +1,112 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using WebStore.Models;
-//using WebStore.Services.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Models;
+using WebStore.Services.Interfaces;
 
-//namespace WebStore.Services
-//{
-//    public class InMemoryEmployesData : IEmployeesData
-//    {
-//        #region Новый объект
-//        private readonly List<Employee> _Employees = new()
-//        {
-//            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
-//            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
-//            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
-//        };
-//        #endregion
+namespace WebStore.Services
+{
+    public class InMemoryEmployesData //: IEmployeesData
+    {
+        #region Новый объект
+        private readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+        #endregion
 
-//        #region старые данные 
-//        //private readonly List<Employee> _Employees = new()
-//        //{
-//        //    new Employee
-//        //    {
-//        //        Id = 1,
-//        //        LastName = "Иванов",
-//        //        FirstName = "Иван",
-//        //        Patronymic = "Иванович",
-//        //        Age = 27,
-//        //        detailsData = new List<Details>()
-//        //                        {
-//        //                            new Details {mail = "ivanov@mail.com", phone = "111-111-1111"},
-//        //                            new Details {mail = "ivanovIvan@mail.com", phone = "111-122-2121"}
-//        //                         }
-//        //    },
-//        //    new Employee
-//        //    {
-//        //        Id = 2,
-//        //        LastName = "Петров",
-//        //        FirstName = "Пётр",
-//        //        Patronymic = "Петрович",
-//        //        Age = 31,
-//        //        detailsData = new List<Details>()
-//        //                        {
-//        //                            new Details {mail = "petrov@mail.com", phone = "222-222-2222"}
-//        //                         }
-//        //    },
-//        //    new Employee
-//        //    {
-//        //        Id = 3,
-//        //        LastName = "Сидоров",
-//        //        FirstName = "Сидор",
-//        //        Patronymic = "Сидорович",
-//        //        Age = 18,
-//        //        detailsData = new List<Details>()
-//        //                        {
-//        //                            new Details {mail = "sidorov@mail.com", phone = "333-333-3333"}
-//        //                         }
-//        //    },
-//        //};
-//        #endregion
+        #region старые данные 
+        //private readonly List<Employee> _Employees = new()
+        //{
+        //    new Employee
+        //    {
+        //        Id = 1,
+        //        LastName = "Иванов",
+        //        FirstName = "Иван",
+        //        Patronymic = "Иванович",
+        //        Age = 27,
+        //        detailsData = new List<Details>()
+        //                        {
+        //                            new Details {mail = "ivanov@mail.com", phone = "111-111-1111"},
+        //                            new Details {mail = "ivanovIvan@mail.com", phone = "111-122-2121"}
+        //                         }
+        //    },
+        //    new Employee
+        //    {
+        //        Id = 2,
+        //        LastName = "Петров",
+        //        FirstName = "Пётр",
+        //        Patronymic = "Петрович",
+        //        Age = 31,
+        //        detailsData = new List<Details>()
+        //                        {
+        //                            new Details {mail = "petrov@mail.com", phone = "222-222-2222"}
+        //                         }
+        //    },
+        //    new Employee
+        //    {
+        //        Id = 3,
+        //        LastName = "Сидоров",
+        //        FirstName = "Сидор",
+        //        Patronymic = "Сидорович",
+        //        Age = 18,
+        //        detailsData = new List<Details>()
+        //                        {
+        //                            new Details {mail = "sidorov@mail.com", phone = "333-333-3333"}
+        //                         }
+        //    },
+        //};
+        #endregion
 
 
-//        private int _CurrentMaxId;
+        private int _CurrentMaxId;
 
-//        public InMemoryEmployesData()
-//        {
-//            _CurrentMaxId = _Employees.Max(i => i.Id);
-//        }
+        public InMemoryEmployesData()
+        {
+            _CurrentMaxId = _Employees.Max(i => i.Id);
+        }
 
-//        public IEnumerable<Employee> GetAll() => _Employees;
+        public IEnumerable<Employee> GetAll() => _Employees;
 
-//        public Employee Get(int id) => _Employees.SingleOrDefault(employee => employee.Id == id);
+        public Employee Get(int id) => _Employees.SingleOrDefault(employee => employee.Id == id);
 
-               
-//        public int Add(Employee employee)
-//        {
-//            if (employee is null) throw new ArgumentNullException(nameof(employee));
 
-//            if (_Employees.Contains(employee)) return employee.Id; 
+        public int Add(Employee employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
 
-//            employee.Id = ++_CurrentMaxId;
-//            _Employees.Add(employee);
+            if (_Employees.Contains(employee)) return employee.Id;
 
-//            return employee.Id;
-//        }
+            employee.Id = ++_CurrentMaxId;
+            _Employees.Add(employee);
 
-//        public void Update(Employee employee)
-//        {
-//            if (employee is null) throw new ArgumentNullException(nameof(employee));
+            return employee.Id;
+        }
 
-//            if(_Employees.Contains(employee)) return; 
+        public void Update(Employee employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
 
-//            var db_item = Get(employee.Id);
-//            if(db_item is null) return;
+            if (_Employees.Contains(employee)) return;
 
-            
-//            db_item.LastName = employee.LastName;
-//            db_item.FirstName = employee.FirstName;
-//            db_item.Patronymic = employee.Patronymic;
-//            db_item.Age = employee.Age;
+            var db_item = Get(employee.Id);
+            if (db_item is null) return;
 
-//        }
 
-//        public bool Delete(int id)
-//        {
-//            var db_item = Get(id);
-//            if (db_item is null) return false;
-//            return _Employees.Remove(db_item);
-//        }
-//    }
-//}
+            db_item.LastName = employee.LastName;
+            db_item.FirstName = employee.FirstName;
+            db_item.Patronymic = employee.Patronymic;
+            db_item.Age = employee.Age;
+
+        }
+
+        public bool Delete(int id)
+        {
+            var db_item = Get(id);
+            if (db_item is null) return false;
+            return _Employees.Remove(db_item);
+        }
+    }
+}
--- WebStore/Services/InSQL/SqlEmployeeData.cs
+++ WebStore/Services/InSQL/SqlEmployeeData.cs
@@ -13,29 +13,57 @@ namespace WebStore.Services.InSQL
     {
         private readonly WebStoreDB _db;
 
-        public SqlEmployeeData(WebStoreDB db) => _db = db;
+        //public SqlEmployeeData(WebStoreDB db) => _db = db;
+
+        private int _CurrentMaxId;
+
+        public SqlEmployeeData(WebStoreDB db)
+        {
+            _db = db;
+            _CurrentMaxId = _db.Employees.Max(i => i.Id);
+        }
 
         public int Add(Domain.Entities.Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            if (_db.Employees.Contains(employee)) return employee.Id;
+
+            employee.Id = ++_CurrentMaxId;
+            _db.Employees.Add(employee);
+
+            return employee.Id;
         }
 
         public bool Delete(int id)
         {
             throw new NotImplementedException();
+
+            //var db_item = Get(id);
+            //if (db_item is null) return false;
+            //return _db.Employees.Remove(db_item);
         }
 
-        public Domain.Entities.Employee Get(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public Domain.Entities.Employee Get(int id) => _db.Employees.SingleOrDefault(employee => employee.Id == id);
+        
 
         public IEnumerable<Domain.Entities.Employee> GetAll() => _db.Employees;
         
 
         public void Update(Domain.Entities.Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            if (_db.Employees.Contains(employee)) return;
+
+            var db_item = Get(employee.Id);
+            if (db_item is null) return;
+
+
+            db_item.LastName = employee.LastName;
+            db_item.FirstName = employee.FirstName;
+            db_item.Patronymic = employee.Patronymic;
+            db_item.Age = employee.Age;
         }
     }
 }
