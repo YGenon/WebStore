c3c27eb191eebd2404084bdcbc1f7d4aa2401d92
YGenon <genonster@gmail.com>
2021-06-15 13:44:48 +0300

    создали миграцию, накатили на БД, подключили сервис identity в Startup.cs

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -11,6 +11,8 @@ using WebStore.Services.Interfaces;
 using WebStore.Data;
 using System;
 using WebStore.Services.InSQL;
+using WebStore.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebStore
 {
@@ -27,6 +29,45 @@ namespace WebStore
         {
             services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(Configuration.GetConnectionString("SQLServer")));
             services.AddTransient<WebStoreDBInitializer>();
+
+#region подключаем Identity
+
+            services.AddIdentity<User, Role>(/*opt => { }*/)
+               .AddEntityFrameworkStores<WebStoreDB>()
+               .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(opt =>
+            {
+#if DEBUG
+                opt.Password.RequireDigit = false;
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredUniqueChars = 3;
+#endif
+
+                opt.User.RequireUniqueEmail = false;
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+                opt.Lockout.AllowedForNewUsers = false;
+                opt.Lockout.MaxFailedAccessAttempts = 10;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+            });
+
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.Cookie.Name = "WebStore.GB";
+                opt.Cookie.HttpOnly = true;
+                opt.ExpireTimeSpan = TimeSpan.FromDays(10);
+
+                opt.LoginPath = "/Account/Login";
+                opt.LogoutPath = "/Account/Logout";
+                opt.AccessDeniedPath = "/Account/AccessDenied";
+
+                opt.SlidingExpiration = true;
+            });
+#endregion
 
             //регистрируем наш сервис
 
