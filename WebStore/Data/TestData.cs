83c778cbac73190297d9693811548778359912a2
YGenon <genonster@gmail.com>
2021-06-25 09:23:03 +0300

    Исправили ошибки в отображении товара

--- WebStore/Data/TestData.cs
+++ WebStore/Data/TestData.cs
@@ -84,7 +84,7 @@ namespace WebStore.Data
             new Product { Id = 6, Name = "Лёгкое голубое поло", Price = 1025, ImageUrl = "product6.jpg", Order = 5, SectionId = 2, BrandId = 1 },
             new Product { Id = 7, Name = "Платье белое", Price = 1025, ImageUrl = "product7.jpg", Order = 6, SectionId = 2, BrandId = 1 },
             new Product { Id = 8, Name = "Костюм кролика", Price = 2025, ImageUrl = "product8.jpg", Order = 7, SectionId = 25, BrandId = 1 },            
-            new Product { Id = 9, Name = "Красное китайское платье", Price = 2025, ImageUrl = "red.jpg", Order = 8, SectionId = 25, BrandId = 1 },
+            new Product { Id = 9, Name = "Красное китайское платье", Price = 2025, ImageUrl = "product9.jpg", Order = 8, SectionId = 25, BrandId = 1 },
             new Product { Id = 10, Name = "Женские джинсы", Price = 2025, ImageUrl = "product10.jpg", Order = 9, SectionId = 25, BrandId = 3 },
             new Product { Id = 11, Name = "Джинсы женские", Price = 2025, ImageUrl = "product11.jpg", Order = 10, SectionId = 25, BrandId = 3 },
             new Product { Id = 12, Name = "Летний костюм", Price = 2025, ImageUrl = "product12.jpg", Order = 11, SectionId = 25, BrandId = 3 },
